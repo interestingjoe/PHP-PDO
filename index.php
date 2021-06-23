44b0d769388fbb4038af256f850547ad95185942
Your Name <you@example.com>
2021-06-22 19:21:06 -0700

    Successfully connecting and pulling data from db.

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <?php
     $host = 'localhost';
     $username = 'root';
-    $password = 'Password4Me!';
+    $password = 'xxxx';
     $db_name = 'helloworld';
     $dsn = 'mysql:host=' . $host . ';dbname=' . $db_name;
 
     $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
     $statement = $pdo->query('SELECT * FROM test');
+
+    while($row = $statement->fetch()) {
+        echo $row->age . '<br>';
+    }
 
 ?>
